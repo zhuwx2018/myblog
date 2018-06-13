ff933d8d649d3f39cf2d40ea7a34131682f6398f
zhuwx <zhuwx2017@gmail.com>
2018-06-13 21:03:51 +0800

    一次修改

--- src/main/java/com/mybatisex/myblog/mapper/NoteInfoMapper.java
+++ src/main/java/com/mybatisex/myblog/mapper/NoteInfoMapper.java
@@ -1,7 +1,23 @@
 package com.mybatisex.myblog.mapper;
 
 import com.mybatisex.myblog.domain.NoteInfo;
+import com.mybatisex.myblog.domain.NoteInfoExample;
 import com.mybatisex.myblog.util.BaseMapper;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface NoteInfoMapper extends BaseMapper<NoteInfo> {
+    int countByExample(NoteInfoExample example);
+
+    int deleteByExample(NoteInfoExample example);
+
+    List<NoteInfo> selectByExampleWithBLOBs(NoteInfoExample example);
+
+    List<NoteInfo> selectByExample(NoteInfoExample example);
+
+    int updateByExampleSelective(@Param("record") NoteInfo record, @Param("example") NoteInfoExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") NoteInfo record, @Param("example") NoteInfoExample example);
+
+    int updateByExample(@Param("record") NoteInfo record, @Param("example") NoteInfoExample example);
 }
